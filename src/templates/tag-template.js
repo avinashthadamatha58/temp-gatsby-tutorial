7510c93b28fb894d216e17150bc06d9b8e9ca1c4
Avinash Thadamatha <avinashthadamatha58@hotmail.com>
2022-05-05 16:34:21 -0400

    fixed tags bug

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -4,22 +4,22 @@ import RecipesList from "../components/RecipesList"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 
-const TagTemplate = props => {
-  const data = useStaticQuery(query)
+const TagTemplate = ({ data, pageContext }) => {
+  const recipes = data.allContentfulRecipe.nodes
   return (
     <Layout>
-      <SEO title={props.pageContext.tag} />
+      <SEO title={pageContext.tag} />
       <main className="page">
-        <h2>{props.pageContext.tag}</h2>
+        <h2>{pageContext.tag}</h2>
         <div className="tag-recipes">
-          <RecipesList recipes={data.allContentfulRecipe.nodes} />
+          <RecipesList recipes={recipes} />
         </div>
       </main>
     </Layout>
   )
 }
 
-const query = graphql`
+export const query = graphql`
   query GetRecipeByTag($tag: String) {
     allContentfulRecipe(
       sort: { fields: title, order: ASC }
